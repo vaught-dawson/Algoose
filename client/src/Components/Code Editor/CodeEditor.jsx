9ae9e78845bab17759d244a044e1c7274d95f0a4
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-27 22:16:44 -0700

    set the code editor's height to match the screen

--- client/src/Components/Code Editor/CodeEditor.jsx
+++ client/src/Components/Code Editor/CodeEditor.jsx
@@ -1,32 +1,25 @@
 import React, { useCallback } from "react";
 import { javascript } from "@codemirror/lang-javascript";
 import { EditorView, basicSetup } from "codemirror";
-import { dracula } from '@uiw/codemirror-theme-dracula';
-import {EditorState, inputHandler} from "@codemirror/state";
+import { dracula } from "@uiw/codemirror-theme-dracula";
+import { EditorState, inputHandler } from "@codemirror/state";
 const CodeEditor = () => {
-    
-	const state = EditorState.create({
-		extensions: [
-			basicSetup,
-			javascript(),
-			dracula,
-		],
-		rangeSet
-	}) 
-	
-	const editorElement = useCallback((editorElement) => {
-        if (editorElement !== null) {
-            let view = new EditorView({
-                state,
-                parent: editorElement,
-            });
-        }
-    }, []);
+  const state = EditorState.create({
+    extensions: [basicSetup, javascript(), dracula],
+    // rangeSet
+  });
 
-    return(
-		<div ref={editorElement}></div>
-	)
-}
+  const editorElement = useCallback((editorElement) => {
+    if (editorElement !== null) {
+      let view = new EditorView({
+        state,
+        parent: editorElement,
+      });
+    }
+  }, []);
+
+  return <div ref={editorElement}></div>;
+};
 
 export default CodeEditor;
 //extend
--- client/src/Views/ProblemPage.jsx
+++ client/src/Views/ProblemPage.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CodeEditor from '../Components/Code Editor/CodeEditor'
-import ProblemDesc from "../Components/ProblemDesc"
+import React from "react";
+import CodeEditor from "../Components/Code Editor/CodeEditor";
+import ProblemDesc from "../Components/ProblemDesc";
 const ProblemPage = () => {
   return (
-    <div className='d-flex flex-row p-4'>
-        <div className="col-6 border border-dark"><ProblemDesc /></div>
-        <div className="col-6 border border-dark"><CodeEditor /> </div>
+    <div className="d-flex flex-row p-4 pb-0">
+      <div className="col-6 border border-dark">
+        <ProblemDesc />
+      </div>
+      <div className="col-6 border border-dark">
+        <CodeEditor />{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProblemPage
+export default ProblemPage;
