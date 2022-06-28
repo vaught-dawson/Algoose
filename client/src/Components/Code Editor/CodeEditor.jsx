8e9572301e217188ecc1b3521d6bdb6932369230
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-28 03:16:29 -0700

    implemented tab into the code editor

--- client/src/Components/Code Editor/CodeEditor.jsx
+++ client/src/Components/Code Editor/CodeEditor.jsx
@@ -1,18 +1,26 @@
 import React, { useCallback } from "react";
 import { javascript } from "@codemirror/lang-javascript";
 import { EditorView, basicSetup } from "codemirror";
+import { keymap } from "@codemirror/view";
+import { indentWithTab } from "@codemirror/commands";
 import { dracula } from "@uiw/codemirror-theme-dracula";
-import { EditorState, inputHandler } from "@codemirror/state";
+import { EditorState, Compartment } from "@codemirror/state";
 const CodeEditor = () => {
   let algoDefault = `/**
   * @param {number[]} nums
   * @param {number} target
   * @return {number[]}
 **/
-function twoSum(nums, target) {\n    \n}`;
-
+function twoSum(nums, target) {\n  \n}`;
+  let tabSize = new Compartment();
   const state = EditorState.create({
-    extensions: [basicSetup, javascript(), dracula],
+    extensions: [
+      basicSetup,
+      javascript(),
+      dracula,
+      keymap.of([indentWithTab]),
+      tabSize.of(EditorState.tabSize.of(4)),
+    ],
     doc: algoDefault,
   });
 
