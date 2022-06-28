5d6bd461911826a7cba60fbbd6ff6d1b300463d5
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-28 02:49:14 -0700

    fix position of the code editor

--- client/src/Views/ProblemPage.jsx
+++ client/src/Views/ProblemPage.jsx
@@ -3,12 +3,15 @@ import CodeEditor from "../Components/Code Editor/CodeEditor";
 import ProblemDesc from "../Components/ProblemDesc";
 const ProblemPage = () => {
   return (
-    <div className="d-flex flex-row p-4 pb-0">
-      <div className="col-6 border border-dark">
+    <div className="d-flex flex-row p-4 pb-0 gap-3">
+      <div className="col-6 border border-dark rounded">
         <ProblemDesc />
       </div>
-      <div className="col-6 border border-dark">
-        <CodeEditor />{" "}
+      <div
+        className="col-6 border border-dark rounded"
+        style={{ width: "calc(50% - 1em)" }}
+      >
+        <CodeEditor />
       </div>
     </div>
   );
