9180d773914c0440a2881f396180319f5d5ed97e
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-29 07:38:29 -0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import Header from "./Components/Navbar/Header"
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import 'bootswatch/dist/darkly/bootstrap.min.css';
-import ProblemPage from "./Views/ProblemPage"
+import Header from "./Components/Navbar/Header";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/darkly/bootstrap.min.css";
+import ProblemPage from "./Views/ProblemPage";
+import { Route, Routes } from "react-router-dom";
 function App() {
-    return <div>
-      <Header />
-      <ProblemPage />
-    </div>;
+	return (
+		<div>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<ProblemPage />
+						</>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
