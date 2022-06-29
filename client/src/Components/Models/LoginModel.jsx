dd2d5cb452101a08ea35e5db52382e7d8ed72c42
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-29 06:51:09 -0700

    converted HTML bootstrap to jsx

--- client/src/Components/Models/LoginModel.jsx
+++ client/src/Components/Models/LoginModel.jsx
@@ -1,47 +1,47 @@
 import React from "react";
+import "bootstrap";
 
 const LoginModel = () => {
-    return (
-        <div class="modal fade" id="loginModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Welcome back!</h5>
-                        <button
-                            type="button"
-                            class="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                            <div class="row mb-3">
-                                <label for="inputEmail" class="col-sm-2 col-form-label">Email:</label>
-                                <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="inputEmail"/>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="inputPassword" class="col-sm-2 col-form-label">Password:</label>
-                                <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="inputPassword"/>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                            Cancel
-                        </button>
-                        <button type="button" class="btn btn-primary">
-                            Login
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="modal fade" id="loginModal">
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+						<h5 className="modal-title">Welcome back!</h5>
+						<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div className="modal-body">
+						<form>
+							<div className="row mb-3">
+								<label htmlFor="inputEmail" className="col-sm-2 col-form-label">
+									Email:
+								</label>
+								<div className="col-sm-10">
+									<input type="email" className="form-control" id="inputEmail" />
+								</div>
+							</div>
+							<div className="row mb-3">
+								<label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+									Password:
+								</label>
+								<div className="col-sm-10">
+									<input type="email" className="form-control" id="inputPassword" />
+								</div>
+							</div>
+						</form>
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+							Cancel
+						</button>
+						<button type="button" className="btn btn-primary">
+							Login
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LoginModel;
--- client/src/Components/Models/RegisterModel.jsx
+++ client/src/Components/Models/RegisterModel.jsx
@@ -1,59 +1,63 @@
-import React from 'react'
+import React from "react";
+import "bootstrap";
 
 const RegisterModel = () => {
-  return (
-    <div class="modal fade" id="registerModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Welcome! Register your account:</h5>
-                <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                ></button>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <div class="row mb-3">
-                        <label for="userName" class="col-sm-4 col-form-label">Username:</label>
-                        <div class="col-sm-8">
-                            <input type="email" class="form-control" id="userName"/>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="email" class="col-sm-4 col-form-label">Email:</label>
-                        <div class="col-sm-8">
-                            <input type="email" class="form-control" id="email"/>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="password" class="col-sm-4 col-form-label">Password:</label>
-                        <div class="col-sm-8">
-                            <input type="email" class="form-control" id="password"/>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="conPassword" class="col-sm-4 col-form-label">Confirm Password:</label>
-                        <div class="col-sm-8">
-                            <input type="email" class="form-control" id="conPassword"/>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    Cancel
-                </button>
-                <button type="button" class="btn btn-primary">
-                    Register
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-  )
-}
+	return (
+		<div className="modal fade" id="registerModal">
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+						<h5 className="modal-title">Welcome! Register your account:</h5>
+						<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div className="modal-body">
+						<form>
+							<div className="row mb-3">
+								<label htmlFor="userName" className="col-sm-4 col-form-label">
+									Username:
+								</label>
+								<div className="col-sm-8">
+									<input type="email" className="form-control" id="userName" />
+								</div>
+							</div>
+							<div className="row mb-3">
+								<label htmlFor="email" className="col-sm-4 col-form-label">
+									Email:
+								</label>
+								<div className="col-sm-8">
+									<input type="email" className="form-control" id="email" />
+								</div>
+							</div>
+							<div className="row mb-3">
+								<label htmlFor="password" className="col-sm-4 col-form-label">
+									Password:
+								</label>
+								<div className="col-sm-8">
+									<input type="email" className="form-control" id="password" />
+								</div>
+							</div>
+							<div className="row mb-3">
+								<label htmlFor="conPassword" className="col-sm-4 col-form-label">
+									Confirm Password:
+								</label>
+								<div className="col-sm-8">
+									<input type="email" className="form-control" id="conPassword" />
+								</div>
+							</div>
+						</form>
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+							Cancel
+						</button>
+						<button type="button" className="btn btn-primary">
+							Register
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default RegisterModel
+export default RegisterModel;
